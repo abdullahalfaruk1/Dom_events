641e1c8772c010dc326ad5366cfb63218811f7c2
Abdullah Al Faruk <alfaruk091@gmail.com>
2026-05-15 08:35:34 +0600

    added a green background file

--- Javascript/events.js
+++ Javascript/events.js
@@ -4,3 +4,19 @@
 function makeYellow() { document.body.style.backgroundColor = 'yellow' }
 
 function makeRed() { document.body.style.backgroundColor = 'red' }
+
+// option 3 for dom event handler : get element by id and then set onclick
+
+const btnMakeBlue = document.getElementById('btn-make-blue');
+// console.log(btnMakeBlue);
+btnMakeBlue.onclick = function makeblue() {
+    document.body.style.backgroundColor = 'blue';
+}
+
+
+  const btnMakeGreen = document.getElementById('btn-make-green');
+        // console.log(btnMakeGreen);
+        btnMakeGreen.onclick = makeGreen;
+        function makeGreen() {
+            document.body.style.backgroundColor = 'green';
+        }
